fd9d942b715f0e6953bfcd121f3bdf91623160df
Juan-CastanedaG <juan.castaneda@epm.com.co>
2026-09-02 07:38:24 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker de la Bitácora de Vuelo Dron.
    Subí el número de versión (bvd-vN) al publicar una versión nueva del index.html. */
-const CACHE = 'bvd-v28';
+const CACHE = 'bvd-v30';
 const TILES = 'bvd-tiles';           // caché de mapa: NO se borra al actualizar
 const ASSETS = [
   './','./index.html','./manifest.json','./icon-192.png','./icon-512.png',
